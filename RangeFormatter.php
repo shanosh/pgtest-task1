34a23318303084f45aa7cf3ae692d7d9f6e93fbe
Shanosh Fernando <shanoshf@yahoo.com>
2013-04-26 20:24:10 +0530

    declared the methods

--- RangeFormatter.php
+++ RangeFormatter.php
@@ -6,19 +6,37 @@
  */
 class RangeFormatter{
 	
-	//TODO create constructor method which accepts start and end parameters
+	
+	/**
+	 * The constructor accepts a positive integer range.
+	 * It will throw out an exception if else
+	 *
+	 * @param Int $start starting number of the range
+	 * @param Int $end ending number of the range
+	 */
+	public function __construct($start, $end){
 	
 		//TODO check if both numbers are integers
-		
+	
 		//TODO check if $end is greater than $start
-		
-		//TODO set the range property
+	
+		//TODO set start and end properties
+
+	
+	}
 		
 	
 	//TODO write the method to iterate through the the range and generate the formatted output
-	
+	public function generateOutput(){
+		
+		//TODO return generated output
+		
+	}
 	
 	//TODO write the method to display the output
+	public function display(){		
+		
+	}
 	
 	
 }
