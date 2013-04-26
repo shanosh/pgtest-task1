453b1f2d0fa0994a3987a3d834b4ef4d55a49fbd
Shanosh Fernando <shanoshf@yahoo.com>
2013-04-26 19:44:34 +0530

    wrote skeletal pseudo code for the RangeFormatter class

--- RangeFormatter.php
+++ RangeFormatter.php
@@ -1,1 +1,24 @@
 <?php
+/**
+ * Range formatter accepts a positive integer range and generates a formatted output
+ * 
+ * @author shanosh
+ */
+class RangeFormatter{
+	
+	//TODO create constructor method which accepts start and end parameters
+	
+		//TODO check if both numbers are integers
+		
+		//TODO check if $end is greater than $start
+		
+		//TODO set the range property
+		
+	
+	//TODO write the method to iterate through the the range and generate the formatted output
+	
+	
+	//TODO write the method to display the output
+	
+	
+}
